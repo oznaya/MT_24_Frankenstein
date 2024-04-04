ec63bfa62d1770f979f6ef9e515a1e162d6dc430
SplitStyx <165952400+SplitStyx@users.noreply.github.com>
2024-04-04 12:54:46 -0500

    U1
    Added servo code example

--- FrankensteinCode.ino
+++ FrankensteinCode.ino
@@ -1,10 +1,17 @@
 const int buttonPin= 13;
 const int ledPin= 2;
 
+#include <Servo.h>
+Servo myservo; //creates servo
+const int buttonPin = 2;
+
 int buttonState= 0;
 
 void setup() {
    pinMode(ledPin, OUTPUT);
+   pinMode(buttonPin, INPUT);
+
+   myservo.attach(9); //attaches servo
    pinMode(buttonPin, INPUT);
 
 }
@@ -16,4 +23,10 @@ void loop() {
   } else {
     digitalWrite(ledPin, LOW);
   }
+   if(digitalRead(buttonPin) == HIGH){
+      myservo.write(180); //tells the angle, 0-180
+   }else{
+      myservo,write(0);
+   }
 }
+
