34520fb503a4d8e11aaabaa88e275bfba61c78dc
Oznaya Juarez <165951990+oznaya@users.noreply.github.com>
2024-04-04 13:04:33 -0500

    U2Oznaya

--- FrankensteinCode.ino
+++ FrankensteinCode.ino
@@ -1,29 +1,29 @@
-const int buttonPin= 13;
+const int buttonPin1= 13;
 const int ledPin= 2;
 
 #include <Servo.h>
 Servo myservo; //creates servo
-const int buttonPin = 2;
+const int buttonPin2= 3;
 
 int buttonState= 0;
 
 void setup() {
    pinMode(ledPin, OUTPUT);
-   pinMode(buttonPin, INPUT);
+   pinMode(buttonPin1, INPUT);
 
    myservo.attach(9); //attaches servo
-   pinMode(buttonPin, INPUT);
+   pinMode(buttonPin2, INPUT);
 
 }
 
 void loop() {
- buttonState = digitalRead(buttonPin);
+ buttonState = digitalRead(buttonPin1);
    if (buttonState == HIGH) {
     digitalWrite(ledPin, HIGH);
   } else {
     digitalWrite(ledPin, LOW);
   }
-   if(digitalRead(buttonPin) == HIGH){
+   if(digitalRead(buttonPin2) == HIGH){
       myservo.write(180); //tells the angle, 0-180
    }else{
       myservo,write(0);
