5d2da95531db6dbe577c06f9173a65a7b9c7fc91
Oznaya Juarez <165951990+oznaya@users.noreply.github.com>
2024-04-22 13:41:38 -0500

    Update FrankensteinCode.ino

--- FrankensteinCode.ino
+++ FrankensteinCode.ino
@@ -1,32 +1,81 @@
-const int buttonPin1= 13;
-const int ledPin= 2;
-
 #include <Servo.h>
-Servo myservo; //creates servo
-const int buttonPin2= 3;
 
-int buttonState= 0;
+Servo firstServo;
+Servo secondServo;
+
+int button1Pin = 8; // Pin for the first button
+int button2Pin = 10; // Pin for the second button
+int button3Pin= 11;
+int button4Pin= 12;
+int button5Pin = 9; // Pin for the third button
+int led1Pin= 4;
+int led2Pin=5;
+
+int firstServoPin = 2; // Pin for the first servo
+int secondServoPin = 3; 
+int lastButtonState = 0; // Pin for the second servo
 
 void setup() {
-   pinMode(ledPin, OUTPUT);
-   pinMode(buttonPin1, INPUT);
+    pinMode(button1Pin, INPUT);
+    pinMode(button2Pin, INPUT);
+    pinMode(button3Pin, INPUT);
+    pinMode(button4Pin, INPUT);
+    pinMode(button5Pin, INPUT);
+    pinMode(led1Pin, OUTPUT);
+    pinMode(led2Pin, OUTPUT);
+    
+    firstServo.attach(firstServoPin);
+    secondServo.attach(secondServoPin);
 
-   myservo.attach(9); //attaches servo
-   pinMode(buttonPin2, INPUT);
-
+    
 }
 
 void loop() {
- buttonState = digitalRead(buttonPin1);
-   if (buttonState == HIGH) {
-    digitalWrite(ledPin, HIGH);
-  } else {
-    digitalWrite(ledPin, LOW);
-  }
-   if(digitalRead(buttonPin2) == HIGH){
-      myservo.write(180); //tells the angle, 0-180
-   }else{
-      myservo,write(0);
-   }
+    // Read the state of the buttons
+    int button1State = digitalRead(button1Pin);
+    int button2State = digitalRead(button2Pin);
+    int button3State = digitalRead(button3Pin);
+    int button4State= digitalRead(button4Pin);
+    int button5State= digitalRead(button5Pin);
+    
+    // Control the first servo
+    if (button1State == LOW){
+      firstServo.write(0);
+    }
+    
+    if (button1State == HIGH) {
+        // First button is high (pressed), set first servo to 100 degrees
+        firstServo.write(100);
+        
+    } 
+    if (button2State == HIGH ) {
+        // Second button
+        firstServo.write(125);
+
+    }
+    if (button3State == HIGH){
+        firstServo.write(150);
+    }
+    if (button4State == HIGH){
+        firstServo.write(180);
+    }
+    // }if (lastButtonState == 0);
+    //    firstServo.write(0);
+
+    
+    
+    // Control the second servo
+    if (button5State == HIGH) {
+        // 5th button is on (pressed), set second servo to 180 degrees
+        digitalWrite(led1Pin,HIGH);
+        delay(500);
+        digitalWrite(led2Pin,LOW);
+        delay(500);
+        digitalWrite (led1Pin,LOW);
+        delay(500);
+       digitalWrite (led2Pin,HIGH);
+        secondServo.write(180);
+        
+    }
 }
 
